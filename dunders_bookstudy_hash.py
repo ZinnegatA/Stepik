51d2295b2e46881b09ac33796db8198591a59930
Ruslan <ruslanibr123@gmail.com>
2022-06-23 19:31:11 +0600

    Initial commit

---
+++ dunders_bookstudy_hash.py
@@ -0,0 +1,11 @@
+class BookStudy:
+    def __init__(self, name, author, year):
+        self.name = name
+        self.author = author
+        self.year = int(year)
+
+    def __hash__(self):
+        return hash((self.name.lower(), self.author.lower()))
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
