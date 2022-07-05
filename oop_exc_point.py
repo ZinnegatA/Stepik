58d8a74d0935c7b76875ab507163cea88d26574d
Ruslan <ruslanibr123@gmail.com>
2022-07-05 11:06:23 +0600

    Initial commit

--- oop_exc_point.py
+++ oop_exc_point.py
@@ -1,11 +1,30 @@
+# 1
+# class Point:
+#     def __init__(self, x, y):
+#         self._x = x
+#         self._y = y
+#
+#
+# pt = Point(1, 2)
+# try:
+#     print(pt.z)
+# except AttributeError:
+#     print('Атрибут с именем z не существует')
+
+# 2
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0):
         self._x = x
         self._y = y
 
 
-pt = Point(1, 2)
+values = input().split()
 try:
-    print(pt.z)
-except AttributeError:
-    print('Атрибут с именем z не существует')
+    pt = Point(int(values[0]), int(values[1]))
+except ValueError:
+    try:
+        pt = Point(float(values[0]), float(values[1]))
+    except ValueError:
+        pt = Point()
+finally:
+    print(f'Point: x = {pt._x}, y = {pt._y}')
